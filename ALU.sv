5bee19678cde34b54b22daa58714aeeb8fac3cb7
Nikhil Agans <nikhil.agans@gmail.com>
2026-08-25 19:08:32 -0400

    Update ALU.sv
    updated code

--- ALU.sv
+++ ALU.sv
@@ -2,35 +2,42 @@
 
 `timescale 1ns/10 ps  // time-unit = 1 ns, precision = 10 ps
 
-module ALU (A, B, opcode, result);
+module ALU (A, B, NZP, ALU_control, result);
   
-  input logic [7:0] A, B;
-  input logic [2:0] opcode;
-  output logic [7:0] result; //need it to be reg because it is changed in the always block
+  input logic [15:0] A, B;
+  input logic[3:0] ALU_control;
+  output logic [15:0] result; //need it to be reg because it is changed in the always block
+  output logic [2:0] NZP;
   
-  // 000 - NOT A 
-  // 001 - AND 
-  // 010 - ADD 
-  // 011 - SUBTRACT 
-  // 100 - OR 
-  // 101 - XOR 
-  // 110 - SHIFT LEFT 
-  // 111 - SHIFT RIGHT 
+  // 0000 - NOT A 
+  // 0001 - AND 
+  // 0010 - ADD 
+  // 0011 - SUBTRACT 
+  // 0100 - OR 
+  // 0101 - XOR 
+  // 0110 - SHIFT LEFT 
+  // 0111 - SHIFT RIGHT 
   
   always_comb //always block is triggered when input changes
     begin 
       
-      case(opcode)
-        3'b000: result = ~A; // NOT
-        3'b001: result = A & B; // AND
-        3'b010: result = A + B; // ADD 
-        3'b011: result = A - B; // SUBTRACT
-        3'b100: result = A | B; // OR
-		3'b101: result = A ^ B; // XOR
-        3'b110: result = A << 1; // SHIFT LEFT 
-        3'b111: result = A >> 1; //SHIFT RIGHT  
-        default: result = 8'b00000000;
-      endcase 
+      case(ALU_control)
+        4'b0000: result = ~A; // NOT
+        4'b0001: result = A & B; // AND
+        4'b0010: result = A + B; // ADD 
+        4'b0011: result = A - B; // SUBTRACT
+        4'b0100: result = A | B; // OR
+		4'b0101: result = A ^ B; // XOR
+        4'b0110: result = A << 1; // SHIFT LEFT 
+        4'b0111: result = A >> 1; //SHIFT RIGHT 
+        
+        default: result = 16'b0;
+      endcase
+      
+      NZP[2] = result[15];
+      NZP[1] = (result == 16'b0);
+      NZP[0] = ~NZP[2] & ~NZP[1];
+      
     end 
 endmodule
   
